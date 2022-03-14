3c31c29a98a3b2988bcb44362d6aa0e99b972079
beombeom1119 <qjarl1119@naver.com>
2022-03-15 00:02:57 +0900

    REST API Path 설정

---
+++ src/main/java/JUST/REST/JustRest/controller/RestAPI.java
@@ -0,0 +1,33 @@
+package JUST.REST.JustRest.controller;
+
+import JUST.REST.JustRest.entity.Language;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class RestAPI {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/langs")
+    public String GetAll()
+    {
+        return "get ALl";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/add")
+    public String Add(@RequestBody Language language)                 //JSON 형식으로 받기 위해서는 @RequestBody를 입력해준다.
+    {
+        return "Add";
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, path = "/update/{id}")                // 동적인 URL 요청은
+    public String Update (@RequestBody Language language, @PathVariable int id)           //  PathVariable을 통해 받는다.
+    {
+        return "Update";
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, path = "/delete/{id}")
+    public String Delete(@PathVariable int id)
+    {
+        return "Delete";
+    }
+
+}
---
+++ src/main/java/JUST/REST/JustRest/entity/Language.java
@@ -0,0 +1,9 @@
+package JUST.REST.JustRest.entity;
+
+public class Language {
+
+    private int id;             // PK
+    private String name;        // 컴 언어 이름
+    private  String level;      // 할 수 있는 레벨
+
+}

32e1adedf5cda434154268704a06045d1bc8e942
beombeom1119 <qjarl1119@naver.com>
2022-03-15 17:45:29 +0900

    REST API Path 설정

--- src/main/java/JUST/REST/JustRest/entity/Language.java
+++ src/main/java/JUST/REST/JustRest/entity/Language.java
@@ -6,4 +6,30 @@ public class Language {
     private String name;        // 컴 언어 이름
     private  String level;      // 할 수 있는 레벨
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
 }
+
+
+

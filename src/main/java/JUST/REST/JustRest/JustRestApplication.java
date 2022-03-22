ae9a888c4d60b9a17f7c99ec30e26f33d70459e2
Beom <qjarl1119@naver.com>
2022-03-23 01:19:47 +0900

    "REST API 재시작"

--- src/main/java/JUST/REST/JustRest/JustRestApplication.java
+++ src/main/java/JUST/REST/JustRest/JustRestApplication.java
@@ -3,7 +3,7 @@ package JUST.REST.JustRest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication				//@SpringBootApplication -> SpringBoot 시작점
+@SpringBootApplication				//@SpringBootApplication -> SpringBoot 시작점!
 public class JustRestApplication {
 
 

cad246e89a38c8feeb08c1e13d549aa0dd47e567
Beom <qjarl1119@naver.com>
2022-03-23 01:12:35 +0900

    "REST API 재시작"

--- src/main/java/JUST/REST/JustRest/JustRestApplication.java
+++ src/main/java/JUST/REST/JustRest/JustRestApplication.java
@@ -3,8 +3,9 @@ package JUST.REST.JustRest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication				//@SpringBootApplication -> SpringBoot 시작점
 public class JustRestApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(JustRestApplication.class, args);
--- src/main/java/JUST/REST/JustRest/repository/PlayerRepository.java
+++
@@ -1,8 +0,0 @@
-package JUST.REST.JustRest.repository;
-
-import JUST.REST.JustRest.entity.Player;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PlayerRepository extends CrudRepository<Player, Long> {
-
-}
